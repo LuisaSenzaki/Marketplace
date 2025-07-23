6083dd7c96a3b08ac66b468cc0624a287369c7ba
LuisaSenzaki <luisa.sayurisen@gmail.com>
2025-07-23 15:07:06 -0300

    feat: área de cases e área para ia

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -86,6 +86,44 @@
 
 
     <!-- Sessão de Cases e IA -->
+     <section class="information">
+        <section class="cases">
+            <h2>Conheça nossos cases</h2>
+            <div class="line"> <hr> </div>
+
+            <button class="seta-esquerda">
+                <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="#D0147A" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+                    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+                </svg>
+            </button>
+
+            <container class="carrossel">
+                <div class="case-item">
+                    <img src="{{ asset('images/case1.jpg') }}" alt="Case 1">
+                </div>
+                <div class="case-item">
+                    <img src="{{ asset('images/case2.jpg') }}" alt="Case 2">
+                </div>
+                <div class="case-item">
+                    <img src="{{ asset('images/case3.jpg') }}" alt="Case 3">
+                </div>
+                <div class="case-item">
+                    <img src="{{ asset('images/case3.jpg') }}" alt="Case 4">
+                </div>
+            </container>
+            <button class="seta-direita">
+                <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="#D0147A" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                </svg>
+            </button>
+        </section>
+
+        <section class="artificial-intelligence">
+            <container class="ai-chat"></container>
+        </section>
+    </section>
+
+
 
     <!-- Sessão Sobre a Xlab -->
      </container>
