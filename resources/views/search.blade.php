32e8baf2a66d6e3384129d083b7a74da32979655
Luisa Senzaki <luisa.sayurisen@gmail.com>
2025-07-29 10:04:17 -0300

    fix(search css): fix the product card of search page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -17,59 +17,59 @@
     <div class="container-search">
 
     <!-- Área de Filtros e Opções -->
-    <section class="left-search">
-        <h2>Ativações</h2>
-        <form method="GET" action="{{ route('search') }}">
-            <div class="filtros">
-                <div class="opcoes-filtros">
-                    <h4>Categorias Especiais</h4>
-                    <label><input type="checkbox" name="categoria[]" value="Eventos Corporativos"> Eventos Corporativos</label>
-                    <label><input type="checkbox" name="categoria[]" value="Eventos de Agronegócio"> Eventos de Agronegócio</label>
-                    <label><input type="checkbox" name="categoria[]" value="Eventos de Saúde"> Eventos de Saúde</label>
-                    <label><input type="checkbox" name="categoria[]" value="Eventos de Beleza e Cosméticos"> Eventos de Beleza e Cosméticos</label>
-                    <label><input type="checkbox" name="categoria[]" value="Eventos Alimentícios"> Eventos Alimentícios</label>
-                </div>
+        <section class="left-search">
+            <h2>Ativações</h2>
+            <form method="GET" action="{{ route('search') }}">
+                <div class="filtros">
+                    <div class="opcoes-filtros">
+                        <h4>Categorias Especiais</h4>
+                        <label><input type="checkbox" name="categoria[]" value="Eventos Corporativos"> Eventos Corporativos</label>
+                        <label><input type="checkbox" name="categoria[]" value="Eventos de Agronegócio"> Eventos de Agronegócio</label>
+                        <label><input type="checkbox" name="categoria[]" value="Eventos de Saúde"> Eventos de Saúde</label>
+                        <label><input type="checkbox" name="categoria[]" value="Eventos de Beleza e Cosméticos"> Eventos de Beleza e Cosméticos</label>
+                        <label><input type="checkbox" name="categoria[]" value="Eventos Alimentícios"> Eventos Alimentícios</label>
+                    </div>
 
-                <div class="opcoes-filtros">
-                    <h4>Sistemas Operacionais</h4>
-                    <label><input type="checkbox" name="sistema_operacional[]" value="Realidade Virtual"> Realidade Virtual</label>
-                    <label><input type="checkbox" name="sistema_operacional[]" value="Games Virtuais"> Games Virtuais</label>
-                    <label><input type="checkbox" name="sistema_operacional[]" value="Cabines e Estações"> Cabines e Estações</label>
-                    <label><input type="checkbox" name="sistema_operacional[]" value="Experiências Interativas"> Experiências Interativas</label>
-                    <label><input type="checkbox" name="sistema_operacional[]" value="ChatBots e Assistentes"> ChatBots e Assistentes</label>
-                </div>
+                    <div class="opcoes-filtros">
+                        <h4>Sistemas Operacionais</h4>
+                        <label><input type="checkbox" name="sistema_operacional[]" value="Realidade Virtual"> Realidade Virtual</label>
+                        <label><input type="checkbox" name="sistema_operacional[]" value="Games Virtuais"> Games Virtuais</label>
+                        <label><input type="checkbox" name="sistema_operacional[]" value="Cabines e Estações"> Cabines e Estações</label>
+                        <label><input type="checkbox" name="sistema_operacional[]" value="Experiências Interativas"> Experiências Interativas</label>
+                        <label><input type="checkbox" name="sistema_operacional[]" value="ChatBots e Assistentes"> ChatBots e Assistentes</label>
+                    </div>
 
-                <div class="opcoes-filtros">
-                    <h4>Modalidade</h4>
-                    <label><input type="checkbox" name="modalidade[]" value="Presencial"> Presencial</label>
-                    <label><input type="checkbox" name="modalidade[]" value="Virtual"> Virtual</label>
-                    <label><input type="checkbox" name="modalidade[]" value="Híbrido"> Híbrido</label>
-                </div>
+                    <div class="opcoes-filtros">
+                        <h4>Modalidade</h4>
+                        <label><input type="checkbox" name="modalidade[]" value="Presencial"> Presencial</label>
+                        <label><input type="checkbox" name="modalidade[]" value="Virtual"> Virtual</label>
+                        <label><input type="checkbox" name="modalidade[]" value="Híbrido"> Híbrido</label>
+                    </div>
 
-                <div class="opcoes-filtros">
-                    <h4>Faixa de Investimento</h4>
-                    <div class="range-container">
-                        <input type="range" id="preco_min" placeholder="Min" min="0" max="10000">
+                    <div class="opcoes-filtros">
+                        <h4>Faixa de Investimento</h4>
+                        <div class="range-container">
+                            <input type="range" id="preco_min" placeholder="Min" min="0" max="10000">
+                        </div>
+                    </div>
+                    <div class="button-container">
+                        <button type="submit">Filtrar</button>
                     </div>
                 </div>
-                <div class="button-container">
-                    <button type="submit">Filtrar</button>
-                </div>
-            </div>
-        </form>
-    </section>
+            </form>
+        </section>
 
     <!-- Lista de Produtos -->
-    <div class="produtos">
-        @foreach($products as $product)
-        <div class="card-produto">
-            <img src="{{ asset('storage/'.$product->image) }}" alt="Produto">
-            <p>{{ $product->name }}</p>
-            <p>R$ {{ number_format($product->price, 2, ',', '.') }}</p>
+        <div class="produtos">
+            @foreach($products as $product)
+            <div class="card-produto">
+                <img src="{{ asset('storage/'.$product->image) }}" alt="Produto">
+                <p class="name-product">{{ $product->name }}</p>
+                <p class="preco">R$ {{ number_format($product->price, 2, ',', '.') }}</p>
+            </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
-</div>
         <script>
   const range = document.getElementById("preco_min");
 
