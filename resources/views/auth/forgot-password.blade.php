9460e213a79f9320af4abee1b37cd760613c7455
Luisa Senzaki <luisa.sayurisen@gmail.com>
2025-08-21 14:39:12 -0300

    fix (cadastro): novo style da tela de redefinir senha

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div style="color: #333; font-family: 'gilroy-light'; font-size: 15px;">
+        {{ __('Esqueceu sua senha? Sem problemas. Basta nos informar seu endereço de e-mail e iremos te enviar um link para redefinir sua senha que permitirá que você escolha uma nova.') }}
     </div>
 
     <!-- Session Status -->
@@ -11,14 +11,14 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-input-label for="email" :value="__('Email')" style="margin-top:20px; color: #D0147A; font-family: 'gilroy-light'; font-size: 15px;"/>
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus style="background: white; border: none; box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);"/>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+            <x-primary-button style="background: #D0147A; color: white; font-family: 'gilroy-light'; font-size: 12px;">
+                {{ __('Resetar Senha') }}
             </x-primary-button>
         </div>
     </form>
